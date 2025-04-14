dcfd9ff05cba8d9e50637d1a79210231b47ae0c8
Andy Skaer <adskaer@gmail.com>
2025-04-14 00:24:01 -0500

    make it easier for copy/paste for new WSL distros

--- alpine/install.sh
+++ alpine/install.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-# Replace name if we have an argument
-DEFAULTVALUE=Alpine
-NAME="${1:-$DEFAULTVALUE}"
-sed "s/$DEFAULTVALUE/$NAME/g" wsl-configuration/wsl-distribution.conf.tmpl > wsl-configuration/wsl-distribution.conf
+TEMP_DIR=/mnt/c/tmp
+# Use first argument for WSL_NAME if provided
+WSL_NAME="${1:-Alpine}"
+
+# Make tmp directory if it doesn't exist
+if [ ! -d ${TEMP_DIR} ]; then
+  echo "${TEMP_DIR} does not exist. Creating..."
+  mkdir -p ${TEMP_DIR};
+fi
+
+# Replace template name
+sed "s/tmplWSL_NAME/$WSL_NAME/g" wsl-configuration/wsl-distribution.conf.tmpl > wsl-configuration/wsl-distribution.conf
 
 # Build image using Dockerfile(that contains necessary wsl files)
-docker build -t alpine_wsl .
+docker build -t linux_wsl .
 
 # Run a container we can export
-docker run -t --name wsl_export alpine_wsl ls
+docker run -t --name wsl_export linux_wsl ls
 
 # Export container to tarball
-docker export wsl_export > /mnt/c/tmp/alpine.tar
+docker export wsl_export > /mnt/c/tmp/linux.tar
 
 # Make it a wsl for double-click installability
-cp /mnt/c/tmp/alpine.tar /mnt/c/tmp/$NAME.wsl
+cp /mnt/c/tmp/linux.tar /mnt/c/tmp/$WSL_NAME.wsl
 
 # Let's clean up after ourselves now
 docker rm wsl_export
-docker rmi alpine_wsl
-rm /mnt/c/tmp/alpine.tar
+docker rmi linux_wsl
+rm /mnt/c/tmp/linux.tar
 rm wsl-configuration/wsl-distribution.conf
